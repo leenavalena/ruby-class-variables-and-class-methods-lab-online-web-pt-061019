1fa1b55e8df4b69138416bbca93ba03d95eb121b
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-24 22:36:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,10 @@ class Song
   end
 
   def self.genre_count
-    
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+
   end
 
   def self.artist_count
